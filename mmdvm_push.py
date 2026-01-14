efca8b634ceb2f67f716a37950cdae9eaf458f69
fnshiwu <fnshiwu@gmail.com>
2026-01-14 22:33:22 +0800

    Update mmdvm_push.py

--- mmdvm_push.py
+++ mmdvm_push.py
@@ -10,7 +10,7 @@ from threading import Semaphore
 # =========================
 # Global Constants
 # =========================
-VERSION = "v3.1.2-S"
+VERSION = "v3.1.5-S"
 CONFIG_FILE = "/etc/mmdvm_push.json"
 LOG_DIR = "/var/log/pi-star/"
 LOCAL_ID_FILE = "/usr/local/etc/nextionUsers.csv"
